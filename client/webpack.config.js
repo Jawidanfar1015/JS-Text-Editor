7329c8c497aebfbd71ceb4d9e6244779928d1c48
Jawid Noori <Hajikaka4030@gmail.com>
2022-08-18 04:02:02 -0700

    package.json updated

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -52,6 +52,7 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
+          exclude: /node_modules/,
           use: ['style-loader', 'css-loader'],
         },
         {
