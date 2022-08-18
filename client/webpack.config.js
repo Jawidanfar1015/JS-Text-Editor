c7c01b65df657e461df9e3f84eafd7e94cb24d45
Jawid Noori <Hajikaka4030@gmail.com>
2022-08-18 01:54:27 -0700

    idb, codemirror and localStorage established

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,66 +1,71 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const WebpackPwaManifest = require('webpack-pwa-manifest')
-const { InjectManifest } = require('workbox-webpack-plugin')
-const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
-    return {
-        mode: "development",
-        entry: {
-            main: ".src/js/index.js",
-            install: "src/js/install.js"
-        },
-        output: {
-            filename: "[name].bundle.js",
-            path: path.resolve(__dirname, "dist"),
-        },
-        plugin: [
-            new HtmlWebpackPlugin({
-                template: "./index.html",
-                title: "JSTE"
-            }),
-            new InjectManifest({
-                swSrc: "./src/src-sw.js",
-                swDest: "./dist/service-worker.js"
-            }),
-            new WebpackPwaManifest({
-                fingerprints: false,
-                inject: true,
-                name: 'JS-Text-Editor',
-                short_name: 'JSTE',
-                description: 'Java Script Text Editor!',
-                background_color: '#362ca4',
-                theme_color: '#571ca1',
-                start_url: '/',
-                publicPath: '/',
-                icons: [
-                  {
-                    src: path.resolve('src/images/logo.png'),
-                    sizes: [96, 128, 192, 256, 384, 512],
-                    destination: path.join('assets', 'icons'),
-                  },
-                ]
-            })
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      //database: './src/js/database.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      // Adding Webpack plugin to generate HTML and inject our bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      // service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // manifest.json
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'text-editor',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
         ],
+      }),
+    ],
 
-        module: {
-            rules: [
-                {
-                    test: /\.css$/i,
-                    use: ['style-loader', 'css-loader']
-                },
-                {
-                    test: /\.m?js$/,
-                    exclude: /node_modules/,
-                    use: {
-                      loader: 'babel-loader',
-                      options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
-                      },
-                    }
-                }
-            ]
-        }
-    }
-}
+    module: {
+      // Adds CSS loaders and babel to webpack.
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            },
+          },
+        },
+      ],
+    },
+  };
+};
--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const app = express()
 
 const PORT = process.env.PORT || 3122;
 
-app.use(express.static('./client/dist'))
+app.use(express.static('../client/dist'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-require('./routes/homerote')(app)
+require('./routes/homeroute')(app)
 
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
